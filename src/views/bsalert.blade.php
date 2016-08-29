882e8a143eb49fe471761066826cfbf58b2f51fc
iConcept <iconcept@iConcepts-MacBook-Pro.local>
2016-08-30 00:34:18 +0700

    Add new style of alert

--- src/views/bsalert.blade.php
+++ src/views/bsalert.blade.php
@@ -1,7 +1,9 @@
-@if (session("error"))
-<div class="alert alert-danger alert-dismissable">
-	<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-	<h4><i class="icon fa fa-ban"></i> Alert!</h4>
-    {{ session("error") }}
-</div>
+@if (count($errors) > 0)
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
 @endif
