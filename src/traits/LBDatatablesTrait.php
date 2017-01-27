7c40addc1461d7f65271a973a3586a030a5d9c82
iConcept <iconcept@MacBook-Pro.local>
2017-01-27 09:27:06 +0700

    Add ability to search

--- src/traits/LBDatatablesTrait.php
+++ src/traits/LBDatatablesTrait.php
@@ -15,7 +15,21 @@ trait LBDatatableTrait {
         {
             if (strlen($column["search"]["value"]) > 0)
             {
-                $query = $query->where($column["data"], "like", "%".$column["search"]["value"]."%");
+                $data = $column["data"];
+                if (strpos($data, ".") === false)
+                {
+                    $query = $query->where($column["data"], "like", "%".$column["search"]["value"]."%");
+                }
+                else
+                {
+                    $comps = explode(".", $data);
+                    $last = $comps[count($comps) - 1];
+                    $data = substr($data, 0, strlen($data) - strlen($last) - 1);
+                    $value = $column["search"]["value"];
+                    $query = $query->whereHas($data, function($query) use ($last, $value) {
+                        $query->where($last, "like", "%$value%");
+                    });
+                }
             }
         }
 
