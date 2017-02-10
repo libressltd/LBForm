77a708a3ae023b06b528c736bdde9744bcdc961b
iConcept <iconcept@MacBook-Pro.local>
2017-02-10 16:00:47 +0700

    Fix bug

--- src/routes.php
+++ src/routes.php
@@ -1,8 +1,8 @@
 <?php
 
-Route::group(["middleware" => ["web"], function() {
+Route::group(["middleware" => ["web"]], function() {
 	Route::get('setLang/{lang}', 'libressltd\lbform\controllers\LBFormController@getSetLang');
-}];
+});
 
 Route::group(["middleware" => ["web", "auth"], 'prefix' => 'lbform', 'namespace' => 'libressltd\lbform\controllers'], function(){
 	Route::get('logout', "LBFormController@logout");
